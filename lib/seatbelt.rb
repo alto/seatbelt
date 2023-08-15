d45df22bde93c2e3e07a20f1babdbda721b7918e
Fiona Sanggang <2127023+fsanggang@users.noreply.github.com>
2023-08-16 10:12:36 +1200

    Remove check for oldskool minitest/unit, this is being phased out
    It is 'ancient' and the minitest gem will no longer load minitest/unit by default:
    https://github.com/minitest/minitest/commit/a2c6c18570f6f0a1bf6af70fe3b6d9599a13fdd6
    
    https://app.asana.com/0/1202486819364117/1205236234507901

--- lib/seatbelt.rb
+++ lib/seatbelt.rb
@@ -12,17 +12,9 @@ if defined?(Test)
 end
 
 if defined?(Minitest)
-  if Minitest::Unit::VERSION[0].to_i < 5
-    class Minitest::Unit::TestCase
-      include Seatbelt::AssertContentType if defined?(ActionController)
-      include AssertJson
-      include Seatbelt::AssertMail if defined?(ActionMailer)
-    end
-  else
-    class Minitest::Test
-      include Seatbelt::AssertContentType if defined?(ActionController)
-      include AssertJson
-      include Seatbelt::AssertMail if defined?(ActionMailer)
-    end
+  class Minitest::Test
+    include Seatbelt::AssertContentType if defined?(ActionController)
+    include AssertJson
+    include Seatbelt::AssertMail if defined?(ActionMailer)
   end
 end
